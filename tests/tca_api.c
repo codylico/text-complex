265102c7240f235b35f738b0dbb94719c3fec491
Cody Licorish <svgmovement@gmail.com>
2020-05-26 20:59:05 -0500

    Update 'Api' test

--- tests/tca_api.c
+++ tests/tca_api.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
   fprintf(stdout, "text-complex version: %s\n", tcmplxA_api_version());
   /* error code "test" */ {
     int pre_v;
-    for (pre_v = -5; pre_v <= 0; ++pre_v) {
+    for (pre_v = -7; pre_v <= 0; ++pre_v) {
       enum tcmplxA_api_error v = (enum tcmplxA_api_error)pre_v;
       char const* v_text = tcmplxA_api_error_toa(v);
       fprintf(stdout, "error value %i: %s\n", v,
