526ddccde4b759483014f23486eedd478bd450fc
Cody Licorish <svgmovement@gmail.com>
2024-10-28 21:01:37 -0400

    `zcvt`: Enable compression tool

--- tools/tca_zcvt.c
+++ tools/tca_zcvt.c
@@ -46,7 +46,62 @@ int do_zsrtostr(FILE *in, FILE* out) {
 
 static
 int do_strrtozs(FILE *in, FILE* out) {
-  return EXIT_FAILURE;
+  unsigned char inbuf[256];
+  unsigned char outbuf[256];
+  int ec = EXIT_SUCCESS;
+  int done = 0;
+  struct tcmplxA_zcvt* const state = tcmplxA_zcvt_new(256, 32768, 1);
+  if (state == NULL)
+    return EXIT_FAILURE;
+  else while (ec == EXIT_SUCCESS) {
+    if (feof(in))
+      break;
+    else if (ferror(in)) {
+      ec = EXIT_FAILURE;
+      break;
+    } else {
+      size_t const in_count = fread(inbuf, sizeof(unsigned char), 256, in);
+      unsigned char const* src;
+      for (src = inbuf; ec == EXIT_SUCCESS && src<inbuf+in_count; ) {
+        size_t retval;
+        int const res = tcmplxA_zcvt_strrtozs
+          (state, &retval, outbuf, 256, &src, inbuf+in_count);
+        if (res < tcmplxA_Success) {
+          ec = EXIT_FAILURE;
+          fprintf(stderr, "error code from conversion:\n\t%s\n",
+              tcmplxA_api_error_toa(res));
+        } else {
+          size_t const out_count = fwrite
+            (outbuf, sizeof(unsigned char), retval, out);
+          if (out_count != retval) {
+            ec = EXIT_FAILURE;
+            fputs("error from file write\n", stderr);
+          }
+        }
+      }
+    }
+  }
+  if (ec != EXIT_SUCCESS)
+    return ec;
+  while (ec == EXIT_SUCCESS && !done) {
+    size_t retval;
+    int const res = tcmplxA_zcvt_delimrtozs(state, &retval, outbuf, 256);
+    if (res < tcmplxA_Success) {
+      ec = EXIT_FAILURE;
+      fprintf(stderr, "error code from conclusion:\n\t%s\n",
+          tcmplxA_api_error_toa(res));
+    } else {
+      size_t const out_count = fwrite
+        (outbuf, sizeof(unsigned char), retval, out);
+      if (out_count != retval) {
+        ec = EXIT_FAILURE;
+        fputs("error from file write\n", stderr);
+      }
+      done = (res == tcmplxA_EOF);
+    }
+  }
+  tcmplxA_zcvt_destroy(state);
+  return ec;
 }
 
 int main(int argc, char**argv) {
